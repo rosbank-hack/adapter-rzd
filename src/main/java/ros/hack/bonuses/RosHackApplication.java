cf64174fa1c4972b6332a33478c7611222b477e5
Blagushin Rostislav <blagushin.r.f@sberbank.ru>
2019-12-07 14:01:35 +0300

    init commit

--- src/main/java/ros/hack/bonuses/RosHackApplication.java
+++ src/main/java/ros/hack/bonuses/RosHackApplication.java
@@ -1,9 +1,13 @@
-package ros.hack.api;
+package ros.hack.bonuses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableKafka
 public class RosHackApplication {
     public static void main(String[] args) {
         SpringApplication.run(RosHackApplication.class, args);
