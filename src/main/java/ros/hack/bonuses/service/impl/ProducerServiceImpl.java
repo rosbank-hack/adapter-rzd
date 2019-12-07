db495b96bb1228625e403a9d13f66d36f9542a37
Blagushin Rostislav <blagushin.r.f@sberbank.ru>
2019-12-07 17:58:50 +0300

    Change topic's name

--- src/main/java/ros/hack/bonuses/service/impl/ProducerServiceImpl.java
+++ src/main/java/ros/hack/bonuses/service/impl/ProducerServiceImpl.java
@@ -1,9 +1,7 @@
 package ros.hack.bonuses.service.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.voteva.Operation;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -17,7 +15,6 @@ public class ProducerServiceImpl implements ProducerService {
     private final KafkaTemplate<String, Operation> kafkaTemplate;
 
     @Override
-    @SneakyThrows(JsonProcessingException.class)
     public void send(String topic, Operation operation) {
         log.debug(operation.toString());
         kafkaTemplate.send(topic, operation);
